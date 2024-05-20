89d780023d88c58a5b89c234546a43b6978416c2
Bruno <brunolemus553@gmail.com>
2024-05-19 21:25:20 -0300

    feat: `Token`

---
+++ src/main/scala/models/Token.scala
@@ -0,0 +1,21 @@
+package com.brunolemus.lox4s
+package models
+
+import models.Token.TokenType
+
+case class Token(tokenType: TokenType, lexeme: String, literal: Any, line: Int)
+
+object Token:
+
+    enum TokenType:
+        case LeftParen, RightParen, LeftBrace, RightBrace, Comma, Dot, Minus,
+            Plus, Semicolon, Slash, Star
+        case Bang, BangEqual, Equal, EqualEqual, Greater, GreaterEqual, Less,
+            LessEqual
+        case Identifier, String, Number
+        case And, Class, Else, False, Fun, For, If, Nil, Or, Print, Return,
+            Super, This, True, Var, While
+        case EndOfFile
+    end TokenType
+
+end Token
